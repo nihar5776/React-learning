ec982c3fe4ee3af8f1ff91320670eff6dd9f15d6
Nihar <yalamanchilinihar66@gmail.com>
2026-06-16 22:15:06 +0530

    Day-1 Completed

---
+++ DAY-1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p>Hello friends. How are you ?</p>
+    </>
+  )
+}
+
+export default App
